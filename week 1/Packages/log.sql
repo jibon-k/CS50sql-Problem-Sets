5676f676f1d6cf610c243ca2bbd4195abc054f2c
Jibon Krishna <47941812+jibon-k@users.noreply.github.com>
2025-10-17 11:15:36 +0600

    completed packages

--- week 1/Packages/log.sql
+++ week 1/Packages/log.sql
@@ -1,7 +1,45 @@
 
 -- *** The Lost Letter ***
+SELECT "address", "type" FROM "addresses"
+WHERE "id" = (
+    SELECT "to_address_id" FROM "packages"
+    WHERE "contents" = 'Congratulatory letter'
+);
 
 -- *** The Devious Delivery ***
+-- First find the type of address the package delivered to
+SELECT "type" FROM "addresses"
+WHERE "id" = (
+    SELECT "address_id" FROM "scans"
+    WHERE "package_id" = (
+        SELECT "id" FROM "packages"
+        WHERE "from_address_id" IS NULL
+        AND "action" = 'Drop'
+    )
+);
+-- Find the content of the package
+SELECT "contents" FROM "packages"
+WHERE "from_address_id" IS NULL;
 
 -- *** The Forgotten Gift ***
-
+-- Find the content type
+SELECT "contents" FROM "packages"
+WHERE "from_address_id" = (
+    SELECT "id" FROM "addresses"
+    WHERE "address" = '109 Tileston Street'
+)
+AND "to_address_id" = (
+    SELECT "id" FROM "addresses"
+    WHERE "address" = '728 Maple Place'
+);
+-- Find the drivers who handled the package
+SELECT "name", "action"
+FROM "drivers"
+JOIN "scans" ON "drivers"."id" = "scans"."driver_id"
+WHERE "package_id" = (
+    SELECT "id" FROM "packages"
+    WHERE "from_address_id" = (
+        SELECT "id" FROM "addresses"
+        WHERE "address" = '109 Tileston Street'
+    )
+);
